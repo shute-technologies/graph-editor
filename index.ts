7d2aaab5b68b5cafb971403adfe5f33a3b619cd5
Cristian F. Shute <cristian.flores@clariti.net>
2020-11-26 18:46:59 -0500

    added the package and index

--- index.ts
+++ index.ts
@@ -1,1 +1,3 @@
 export * from './src/shute-technologies/modules/editor/geeEngine';
+export * from './src/shute-technologies/modules/runtime/geePlaybackEngine';
+export * from './src/shute-technologies/modules/common/geeInterfaces';
