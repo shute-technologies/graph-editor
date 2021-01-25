76947c6711eed64b1091f75b1f2e3adefc69221e
Cristian F. Shute <cristian.flores@clariti.net>
2021-01-25 18:54:06 -0500

    exposing classes

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+export * from './src/shute-technologies/modules/editor/geeGraph';
 export * from './src/shute-technologies/modules/editor/geeEngine';
 export * from './src/shute-technologies/modules/runtime/geePlaybackEngine';
 export * from './src/shute-technologies/modules/common/geeInterfaces';
